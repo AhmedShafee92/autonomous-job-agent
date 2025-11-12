4a9b0efaa07044aa70f57dd9e7c3f8d9c5b7b2cb
Ahmed-Shafee <Ahmed_92a@hotmail.com>
2025-11-12 14:32:37 +0200

    update the client commection with the server for the searching process

--- src/MainApp.java
+++ src/MainApp.java
@@ -3,7 +3,8 @@ import view.AuthView;
 
 import javax.swing.*;
 
-public class MainApp {
+public class MainApp 
+{
     public static void main(String[] args) 
     {
         SwingUtilities.invokeLater(() -> {
